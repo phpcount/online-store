1438697d278427643a696190e01692a6cd19c0f8
harkley505 <boyko.v@introvert.bz>
2022-07-06 14:09:34 +0700

    Added and set up webpack for site (uncompiled files in a folder assets)

---
+++ assets/section-admin.js
@@ -0,0 +1,19 @@
+const $ = require('jquery');
+
+require('bootstrap');
+
+global.$ = global.JQuery = $;
+
+require('jquery.easing');
+require('chart.js');
+
+require('./js/section/admin/theme/sb-admin-2');
+
+// importing all styles
+import './css/section/admin/libs.scss';
+
+import './css/section/admin/sb-admin-2.css';
+import './css/section/admin/styles.css';
+
+// importing all scripts
+
